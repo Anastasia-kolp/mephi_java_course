4ecaff2c813d4f29385b3ebad9eda7663bab3c79
Anastasia-kolp <nastena.kolpakova@mail.ru>
2021-10-10 16:58:43 +0300

    lab2 finished

---
+++ lab2/src/main/java/ru/mephi/lab2/WordsCounter.java
@@ -0,0 +1,20 @@
+package ru.mephi.lab2;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+class WordsCounter {
+    public static void main(String[] args){
+        System.out.print("Enter the string >>> ");
+        Scanner input = new Scanner(System.in);
+        String[] counter = input.nextLine().trim().replaceAll("\\s+", " ").split(" ");
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (String str: counter){
+            Object value = map.get(str);
+            map.put(str, (value==null) ? 1 : ((int)value+1));
+        }
+        for (HashMap.Entry entry : map.entrySet()) {
+            System.out.println(entry.getKey()+": "+ entry.getValue());
+        }
+    }
+}
